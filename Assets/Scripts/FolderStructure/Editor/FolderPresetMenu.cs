4f134204780866878004586f35a635689c23212e
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-05-14 17:03:14 -0700

    Add options

--- Assets/Scripts/FolderStructure/Editor/FolderPresetMenu.cs
+++ Assets/Scripts/FolderStructure/Editor/FolderPresetMenu.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 
 namespace FolderStructure.Editor {
     public static class FolderPresetMenu {
+        // Should we generate a sandbox folder?
+        private static readonly bool doSandbox = true;
+        
+        // Should we generate .keep files for version control?
+        private static readonly bool makeKeepFiles = true;
+        
         [MenuItem("Assets/Unity Wizard/Run Folder Preset (temp)", true)]
         private static bool ValidateRunnable() => Selection.activeObject is FolderPresetSO || Selection.activeObject is FolderPresetGroupSO;
 
         [MenuItem("Assets/Unity Wizard/Run Folder Preset (temp)", false)]
         private static void Run() {
+            FolderService folderService;
+            if (doSandbox) {
+                folderService = new FolderService(Path.Join(Application.dataPath, "SandboxedFolder"), Debug.LogWarning,
+                    Debug.Log, makeKeepFiles);
+            } else {
+                folderService = new FolderService(Application.dataPath, Debug.LogWarning, Debug.Log, makeKeepFiles);
+            }
+
             if (Selection.activeObject is FolderPresetSO presetSO) {
-                CreateFoldersFromPreset(presetSO);
+                CreateFoldersFromPreset(presetSO, folderService);
             } else if (Selection.activeObject is FolderPresetGroupSO presetGroupSO) {
                 foreach (FolderPresetSO preset in presetGroupSO.IncludedPresets) {
-                    CreateFoldersFromPreset(preset);
+                    CreateFoldersFromPreset(preset, folderService);
                 }
             }
             AssetDatabase.Refresh();
         }
 
-        private static void CreateFoldersFromPreset(FolderPresetSO presetSO) {
-            var service = new FolderService(Path.Join(Application.dataPath, "SandboxedFolder"), Debug.LogWarning);
-            service.BatchCreateFolders(presetSO.folderPaths);
+        private static void CreateFoldersFromPreset(FolderPresetSO presetSO, FolderService folderService) {
+            folderService.BatchCreateFolders(presetSO.folderPaths);
         }
     }
 }
